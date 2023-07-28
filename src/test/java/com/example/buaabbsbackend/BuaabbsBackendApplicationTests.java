4c8e138361fd96dd13ba08815f11f0c4203a3afe
AlpaCa <2604307870@qq.com>
2023-07-28 21:46:10 +0800

    项目初始化

---
+++ src/test/java/com/example/buaabbsbackend/BuaabbsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buaabbsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuaabbsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
